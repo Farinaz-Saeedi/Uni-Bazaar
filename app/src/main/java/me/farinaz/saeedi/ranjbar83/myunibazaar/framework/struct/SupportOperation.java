0b37126dc893d8ccf213ac833952c4fa89265da8
fery <fsaeediranjbar@gmail.com>
2026-06-21 19:58:14 +0330

    feat: add SupportOperation data model class

---
+++ app/src/main/java/me/farinaz/saeedi/ranjbar83/myunibazaar/framework/struct/SupportOperation.java
@@ -0,0 +1,40 @@
+package me.farinaz.saeedi.ranjbar83.myunibazaar.framework.struct;
+
+public class SupportOperation {
+    private float supportPercentOperation;
+    private float supportPercentStore;
+    private float supportPercentDiscount;
+    private float supportPercentBadge;
+
+    public float getSupportPercentOperation() {
+        return supportPercentOperation;
+    }
+
+    public void setSupportPercentOperation(float supportPercentOperation) {
+        this.supportPercentOperation = supportPercentOperation;
+    }
+
+    public float getSupportPercentStore() {
+        return supportPercentStore;
+    }
+
+    public void setSupportPercentStore(float supportPercentStore) {
+        this.supportPercentStore = supportPercentStore;
+    }
+
+    public float getSupportPercentDiscount() {
+        return supportPercentDiscount;
+    }
+
+    public void setSupportPercentDiscount(float supportPercentDiscount) {
+        this.supportPercentDiscount = supportPercentDiscount;
+    }
+
+    public float getSupportPercentBadge() {
+        return supportPercentBadge;
+    }
+
+    public void setSupportPercentBadge(float supportPercentBadge) {
+        this.supportPercentBadge = supportPercentBadge;
+    }
+}
