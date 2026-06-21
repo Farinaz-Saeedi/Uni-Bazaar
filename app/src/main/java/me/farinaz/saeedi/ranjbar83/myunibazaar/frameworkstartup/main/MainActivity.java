c830695fe8b3343627f7ac1449da79e350c35812
fery <fsaeediranjbar@gmail.com>
2026-06-21 18:07:04 +0330

    feat: create initial package structure

--- app/src/main/java/me/farinaz/saeedi/ranjbar83/myunibazaar/frameworkstartup/main/MainActivity.java
+++ app/src/main/java/me/farinaz/saeedi/ranjbar83/myunibazaar/frameworkstartup/main/MainActivity.java
@@ -1,4 +1,4 @@
-package me.farinaz.saeedi.ranjbar83.myunibazaar;
+package me.farinaz.saeedi.ranjbar83.myunibazaar.frameworkstartup.main;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import me.farinaz.saeedi.ranjbar83.myunibazaar.R;
 
 public class MainActivity extends AppCompatActivity {
 
