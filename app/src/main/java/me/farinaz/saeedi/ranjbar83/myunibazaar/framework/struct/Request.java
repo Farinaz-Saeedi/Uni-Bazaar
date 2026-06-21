519863f339fc45babab2f3e798c376be91d1ce04
fery <fsaeediranjbar@gmail.com>
2026-06-21 20:02:14 +0330

    feat: add Request data model class

---
+++ app/src/main/java/me/farinaz/saeedi/ranjbar83/myunibazaar/framework/struct/Request.java
@@ -0,0 +1,42 @@
+package me.farinaz.saeedi.ranjbar83.myunibazaar.framework.struct;
+
+public class Request {
+
+    private String storeName;
+    private String date;
+    private String description;
+    private String image;
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
+
