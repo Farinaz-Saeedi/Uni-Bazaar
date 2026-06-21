e2f8841bc30ab24d3258fe3193f21aaebc8dddef
fery <fsaeediranjbar@gmail.com>
2026-06-21 19:59:45 +0330

    feat: add Support data model class

---
+++ app/src/main/java/me/farinaz/saeedi/ranjbar83/myunibazaar/framework/struct/Support.java
@@ -0,0 +1,60 @@
+package me.farinaz.saeedi.ranjbar83.myunibazaar.framework.struct;
+
+public class Support {
+
+    private int supportCode;
+    private String firsName;
+    private String lastName;
+    private String image;
+    private String password;
+    private String date;
+
+    public int getSupportCode() {
+        return supportCode;
+    }
+
+    public void setSupportCode(int supportCode) {
+        this.supportCode = supportCode;
+    }
+
+    public String getFirsName() {
+        return firsName;
+    }
+
+    public void setFirsName(String firsName) {
+        this.firsName = firsName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
