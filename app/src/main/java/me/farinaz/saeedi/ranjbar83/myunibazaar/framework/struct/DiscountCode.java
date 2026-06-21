c849c5e6c590a9e65cdf379defa24e10bbcc5000
fery <fsaeediranjbar@gmail.com>
2026-06-21 20:04:08 +0330

    feat: add DiscountCode data model class

---
+++ app/src/main/java/me/farinaz/saeedi/ranjbar83/myunibazaar/framework/struct/DiscountCode.java
@@ -0,0 +1,34 @@
+package me.farinaz.saeedi.ranjbar83.myunibazaar.framework.struct;
+
+public class DiscountCode {
+
+    private int codId;
+    private String amount;
+    private String scope;
+
+
+    public int getCodId() {
+        return codId;
+    }
+
+    public void setCodId(int codId) {
+        this.codId = codId;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+}
+
