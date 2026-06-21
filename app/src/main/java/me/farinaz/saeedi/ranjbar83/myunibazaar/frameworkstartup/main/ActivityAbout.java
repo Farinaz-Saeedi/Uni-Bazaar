9528f56dea996d720165ae23dc87a3a83bb4c7f2
fery <fsaeediranjbar@gmail.com>
2026-06-21 19:47:09 +0330

    feat: implement about screen activity with toolbar and styling

--- app/src/main/java/me/farinaz/saeedi/ranjbar83/myunibazaar/frameworkstartup/main/ActivityAbout.java
+++ app/src/main/java/me/farinaz/saeedi/ranjbar83/myunibazaar/frameworkstartup/main/ActivityAbout.java
@@ -1,4 +1,69 @@
 package me.farinaz.saeedi.ranjbar83.myunibazaar.frameworkstartup.main;
 
-public class ActivityAbout {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import me.farinaz.saeedi.ranjbar83.myunibazaar.R;
+import me.farinaz.saeedi.ranjbar83.myunibazaar.framework.core.G;
+
+public class ActivityAbout extends AppCompatActivity {
+
+    TextView txt1;
+    TextView txt2;
+    TextView txt3;
+    TextView txt4;
+    TextView txt5;
+    TextView txt6;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_about);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.cv_recommend), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        txt1 = findViewById(R.id.txt11);
+        txt2 = findViewById(R.id.txt2);
+        txt3 = findViewById(R.id.txt3);
+        txt4 = findViewById(R.id.txt4);
+        txt5 = findViewById(R.id.txt5);
+        txt6 = findViewById(R.id.txt6);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tb_about);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        txt1.setTypeface(G.fontTitle);
+        txt2.setTypeface(G.font1);
+        txt3.setTypeface(G.font2);
+        txt4.setTypeface(G.font2);
+        txt5.setTypeface(G.font2);
+        txt6.setTypeface(G.font2);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+
+
+    }
 }
