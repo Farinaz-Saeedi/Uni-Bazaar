7132e671d48e2f64434cfb3de1cd1433ebc0c6d6
fery <fsaeediranjbar@gmail.com>
2026-06-21 20:03:13 +0330

    feat: add Product data model class

---
+++ app/src/main/java/me/farinaz/saeedi/ranjbar83/myunibazaar/framework/struct/Product.java
@@ -0,0 +1,43 @@
+package me.farinaz.saeedi.ranjbar83.myunibazaar.framework.struct;
+
+public class Product {
+
+    private String title;
+    private String price;
+    private String dis;
+    private String image;
+
+
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDis() {
+        return dis;
+    }
+
+    public void setDis(String dis) {
+        this.dis = dis;
+    }
+}
