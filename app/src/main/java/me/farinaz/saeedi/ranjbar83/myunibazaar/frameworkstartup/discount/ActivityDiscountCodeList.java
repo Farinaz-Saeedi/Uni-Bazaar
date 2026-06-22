1def16ea5aba639acceddaaf0abf9f8336ee6c1d
fery <fsaeediranjbar@gmail.com>
2026-06-22 15:41:21 +0330

    feat: add reusable base adapter for discount code list views

---
+++ app/src/main/java/me/farinaz/saeedi/ranjbar83/myunibazaar/frameworkstartup/discount/ActivityDiscountCodeList.java
@@ -0,0 +1,47 @@
+package me.farinaz.saeedi.ranjbar83.myunibazaar.frameworkstartup.discount;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import me.farinaz.saeedi.ranjbar83.myunibazaar.R;
+
+import java.util.ArrayList;
+
+
+
+public abstract class ActivityDiscountCodeList<T> extends ArrayAdapter<T> {
+    private LayoutInflater inflater;
+
+    public ActivityDiscountCodeList(Context context, ArrayList<T> list) {
+        super(context, 0, list);
+        inflater = LayoutInflater.from(context);
+    }
+
+    public static class ViewHolder {}
+
+    public abstract ViewHolder assign(View convertView);
+    public abstract void fill(ViewHolder upcastedViewHolder, T item);
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        T item = getItem(position);
+
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.adapter_discount_code, parent, false);
+
+            viewHolder = assign(convertView);
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        fill(viewHolder, item);
+
+        return convertView;
+    }
+}
