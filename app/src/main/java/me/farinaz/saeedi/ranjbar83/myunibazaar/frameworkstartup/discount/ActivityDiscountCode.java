ee5befa89267834da8be2b71e3309cdd68cf4cea
fery <fsaeediranjbar@gmail.com>
2026-06-22 15:49:52 +0330

    feat: implement discount code list activity with database loading and adapter binding

---
+++ app/src/main/java/me/farinaz/saeedi/ranjbar83/myunibazaar/frameworkstartup/discount/ActivityDiscountCode.java
@@ -0,0 +1,67 @@
+package me.farinaz.saeedi.ranjbar83.myunibazaar.frameworkstartup.discount;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import me.farinaz.saeedi.ranjbar83.myunibazaar.R;
+import me.farinaz.saeedi.ranjbar83.myunibazaar.framework.adapter.AdapterDiscountCodeList;
+import me.farinaz.saeedi.ranjbar83.myunibazaar.framework.core.G;
+import me.farinaz.saeedi.ranjbar83.myunibazaar.framework.database.DatabaseDiscountCodeList;
+
+
+public class ActivityDiscountCode extends AppCompatActivity {
+
+    private int id;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_discount_code_list_view);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.cv_recommend), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Toolbar toolbar = findViewById(R.id.toolbar_discount);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+
+        }
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+
+        if (intent.hasExtra("id")){
+            id = bundle.getInt("id");
+        }
+
+        ListView listCode = findViewById(R.id.list_code);
+
+        DatabaseDiscountCodeList.getCodeInfo(id);
+        AdapterDiscountCodeList adapter = new AdapterDiscountCodeList(this, G.arrayListDiscountCode);
+        listCode.setAdapter(adapter);
+
+
+    }
+}
