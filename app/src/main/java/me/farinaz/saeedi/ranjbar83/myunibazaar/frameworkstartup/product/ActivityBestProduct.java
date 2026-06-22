19ccfa7739d902d23b3cd9d2aeaa007859e6f14b
fery <fsaeediranjbar@gmail.com>
2026-06-22 15:51:46 +0330

    feat: implement best selling products activity with database query and list binding

---
+++ app/src/main/java/me/farinaz/saeedi/ranjbar83/myunibazaar/frameworkstartup/product/ActivityBestProduct.java
@@ -0,0 +1,57 @@
+package me.farinaz.saeedi.ranjbar83.myunibazaar.frameworkstartup.product;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import me.farinaz.saeedi.ranjbar83.myunibazaar.R;
+import me.farinaz.saeedi.ranjbar83.myunibazaar.framework.adapter.AdapterProductList;
+import me.farinaz.saeedi.ranjbar83.myunibazaar.framework.core.G;
+import me.farinaz.saeedi.ranjbar83.myunibazaar.framework.database.DatabaseProductList;
+
+public class ActivityBestProduct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_best_product_list);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar_best);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+
+        }
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        ListView listProducts = findViewById(R.id.list_best_product);
+
+
+        DatabaseProductList.getBestSellingProducts();
+        AdapterProductList adapter = new AdapterProductList(this, G.arrayListProduct);
+        listProducts.setAdapter(adapter);
+
+
+    }
+}
