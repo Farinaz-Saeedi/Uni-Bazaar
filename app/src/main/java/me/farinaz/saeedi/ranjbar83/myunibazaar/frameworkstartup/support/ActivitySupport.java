f54c6620799664aa8e4444d8eed6c19bc3e87320
fery <fsaeediranjbar@gmail.com>
2026-06-22 16:18:12 +0330

    feat: implement support list activity with database loading and adapter integration

---
+++ app/src/main/java/me/farinaz/saeedi/ranjbar83/myunibazaar/frameworkstartup/support/ActivitySupport.java
@@ -0,0 +1,56 @@
+package me.farinaz.saeedi.ranjbar83.myunibazaar.frameworkstartup.support;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import me.farinaz.saeedi.ranjbar83.myunibazaar.R;
+import me.farinaz.saeedi.ranjbar83.myunibazaar.framework.adapter.AdapterSupportList;
+import me.farinaz.saeedi.ranjbar83.myunibazaar.framework.core.G;
+import me.farinaz.saeedi.ranjbar83.myunibazaar.framework.database.DatabaseSupportList;
+
+
+public class ActivitySupport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_support_list);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.cv_recommend), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Toolbar toolbar = findViewById(R.id.toolbar_support);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+
+        }
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        ListView listSupport = findViewById(R.id.list_support);
+
+        DatabaseSupportList.getData();
+        AdapterSupportList adapter = new AdapterSupportList(this, G.arrayListSupport);
+        listSupport.setAdapter(adapter);
+
+
+    }
+}
