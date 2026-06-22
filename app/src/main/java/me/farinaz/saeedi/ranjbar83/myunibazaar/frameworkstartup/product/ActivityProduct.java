ee47d9483717dafb7fd94536144200f05c1687d1
fery <fsaeediranjbar@gmail.com>
2026-06-22 15:54:41 +0330

    feat: implement product list activity with user-based data loading and adapter binding

---
+++ app/src/main/java/me/farinaz/saeedi/ranjbar83/myunibazaar/frameworkstartup/product/ActivityProduct.java
@@ -0,0 +1,66 @@
+package me.farinaz.saeedi.ranjbar83.myunibazaar.frameworkstartup.product;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import me.farinaz.saeedi.ranjbar83.myunibazaar.R;
+import me.farinaz.saeedi.ranjbar83.myunibazaar.framework.adapter.AdapterProductList;
+import me.farinaz.saeedi.ranjbar83.myunibazaar.framework.core.G;
+import me.farinaz.saeedi.ranjbar83.myunibazaar.framework.database.DatabaseProductList;
+
+public class ActivityProduct extends AppCompatActivity {
+
+    private int id;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_product_list);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Toolbar toolbar = findViewById(R.id.toolbar_view);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+
+        }
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+
+        if (intent.hasExtra("id")){
+            id = bundle.getInt("id");
+        }
+        ListView listProducts = findViewById(R.id.list_product);
+
+
+        DatabaseProductList.getViewedProducts(id);
+        AdapterProductList adapter = new AdapterProductList(this, G.arrayListProduct);
+        listProducts.setAdapter(adapter);
+
+
+    }
+}
